bf1830b98b9d428287608b793fc1adb7ad0625bb
Lim Kha Shing <kslim5703@gmail.com>
2022-04-01 02:26:57 +0800

    add rule to grant permission

--- app/src/androidTest/java/com/kslimweb/ipolyglot/MainActivityFunctionalityTest.kt
+++ app/src/androidTest/java/com/kslimweb/ipolyglot/MainActivityFunctionalityTest.kt
@@ -1,15 +1,16 @@
 package com.kslimweb.ipolyglot
 
+import android.Manifest
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
-import org.hamcrest.CoreMatchers.not
+import androidx.test.rule.GrantPermissionRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityFunctionalityTest {
@@ -17,6 +18,10 @@ class MainActivityFunctionalityTest {
     @Rule
     @JvmField
     val rule = ActivityTestRule(MainActivity::class.java)
+
+    @Rule
+    @JvmField
+    val permissionRule = GrantPermissionRule.grant(Manifest.permission.RECORD_AUDIO)
 
     /**
      * TODO Test Component Functionality
